17e536b7d49582f112dd15465eb18499074f8e47
JUANMIGUELPAZ\migue <juanmpaz2@gmail.com>
2021-11-17 22:30:18 -0600

    subida

--- Usuarios/crear_usuarios.php
+++ Usuarios/crear_usuarios.php
@@ -44,33 +44,4 @@ if (pg_send_query($con, $sql_query)) {
   }
 
 ?>
-
-
-
-$sql_query = "SELECT  public.ft_create_user_account('".$postdata."')";
-
-if (pg_send_query($con, $sql_query)) {
-    $res=pg_get_result($con);
-    if ($res) {
-      $state = pg_result_error_field($res, PGSQL_DIAG_SQLSTATE);
-      if ($state==0) {
-            $datos = pg_fetch_all($res);
-            $variable = $datos[0]["ft_create_user_account"]; 
-            echo  $variable ;    
-            }
-      else {
-        // some error happened
-        if ($state == 23505) { // unique_violation
-            $resultado = array('estado' => $state);
-            echo json_encode($resultado) ;    
-        }
-        else {
-            $resultado = array('estado' => $state);
-            echo json_encode($resultado) ;
-         // process other errors
-        }
-      }
-    }  
-  }
-
-?>
+ 
